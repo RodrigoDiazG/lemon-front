9421a885c1cd449375dca5b5be4d632c0dc5fe4b
Rodrigo Diaz Guerrero <rodrigo.s.diaz@clarochile.cl>
2020-07-08 13:44:22 -0400

    Prueba Lemontech 1

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,5 +1,5 @@
 /**
- * This its the config environment file
+ * This its the config environment file  
  * The docker replaces the file automatically if the react app deploy in production
  */
 export class Config {
