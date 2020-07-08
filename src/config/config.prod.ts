4bcebf6df77461057fecf6a09e1bc4ed24cbad88
Rodrigo Diaz Guerrero <rodrigo.s.diaz@clarochile.cl>
2020-07-07 21:22:40 -0400

    Prueba Final2

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -3,5 +3,5 @@
  * The docker replaces the file automatically if the react app deploy in production
  */
 export class Config {
-    public static API: string = 'http://lemon-backend-srv:80/api/v1'
+    public static API: string = 'http://a96f8c8b2b1a04611863545b09b01718-1745294384.us-east-2.elb.amazonaws.com:5001/api/v1'
 }
